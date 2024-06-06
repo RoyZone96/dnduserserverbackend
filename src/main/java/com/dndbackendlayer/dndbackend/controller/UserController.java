3b1b96ccef8014a8ad62f75505ad4f21183834af
Matthew Wiewel <mjvwiewel@gmail.com>
2024-06-06 15:48:36 -0500

    changes during chat

--- src/main/java/com/dndbackendlayer/dndbackend/controller/UserController.java
+++ src/main/java/com/dndbackendlayer/dndbackend/controller/UserController.java
@@ -5,48 +5,48 @@ import com.dndbackendlayer.dndbackend.repo.UserRepo;
 import com.dndbackendlayer.dndbackend.exception.UserNotFoundException;
 
 import java.util.List;
+
+import com.dndbackendlayer.dndbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
+@RequestMapping("users")
+@Validated //see about this later
 public class UserController {
-    @Autowired
-    private UserRepo userRepo;
 
-    @PostMapping("/user")
-    User newUser(@RequestBody User newUser) {
-        return userRepo.save(newUser);
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/createUser")
+    ResponseEntity<User> newUser(@RequestBody User newUser) {
+        return new ResponseEntity<>(userService.newUser(newUser), HttpStatus.CREATED);
     }
 
     @GetMapping("/users")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     List<User> getAllUsers() {
-        return userRepo.findAll();
+        return userService.getAllUsers();
     }
 
     @GetMapping("user/id/{id}")
     User getUserById(@PathVariable Long id) {
-        return userRepo.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
+        return userService.getUserById(id);
     }
 
     @GetMapping("/user/username/{username}")
     User getUserByUsername(@PathVariable String username) {
-        return userRepo.findByUsername(username)
-            .orElseThrow(() -> new UserNotFoundException(username));
+        return userService.getUserByUsername(username);
     }
 
     @GetMapping("/user/email/{email}")
     User getUserByEmail(@PathVariable String email) {
-        return userRepo.findByEmail(email)
-            .orElseThrow(() -> new UserNotFoundException(email));
+        return userService.getUserByEmail(email);
     }
 
 }
