7486137301a144e521bd91c55cf1820a1b2bc9dd
Matthew Wiewel <mjvwiewel@gmail.com>
2024-07-10 12:31:53 -0500

    added getters/setters

--- src/main/java/com/dndbackendlayer/dndbackend/model/AuthRequest.java
+++ src/main/java/com/dndbackendlayer/dndbackend/model/AuthRequest.java
@@ -12,5 +12,17 @@ import lombok.NoArgsConstructor;
 public class AuthRequest {
     private String username;
     private String password;
-    
+
+    public String getPassword() {
+        return password;
+    }
+    public String getUsername () {
+        return username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
