bdb7da1bd9fcc792384000df0d7045fb90d7bb54
Briceson <royzone4@gmail.com>
2024-07-18 23:10:27 -0400

    it get's the user_id of the user to the bookmark database

--- src/main/java/com/dndbackendlayer/dndbackend/model/Bookmark.java
+++ src/main/java/com/dndbackendlayer/dndbackend/model/Bookmark.java
@@ -21,10 +21,18 @@ public class Bookmark {
     private String url;
 
     private Date createDate;
-
+ 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private User user;
+
+
+    @Column(name = "user_id")
+    private long user_id;
+
+  
+   //private long user_id;
+
 
     // Generate getters and setters
     public long getId() {
@@ -52,4 +60,28 @@ public class Bookmark {
         this.createDate = createDate;
     }
 
+    // public Long getUserId() {
+    //     if (this.user != null) {
+    //         return this.user.getId(); // Assuming User class has getId() method returning Long.
+    //     } else {
+    //         return null; // Handle case where user is null.
+    //     }
+    // }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+    
 }
